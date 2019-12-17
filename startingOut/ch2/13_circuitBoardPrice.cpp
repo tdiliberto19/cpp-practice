2f0798bed954e7cb47ebeeab12f350da6e85c569
Timothy <tdiliberto19@berkeley.edu>
2019-12-16 22:51:49 -0800

    added 13-16

---
+++ startingOut/ch2/13_circuitBoardPrice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const double PERCENT_PROFIT = 1.35;
+    double cost = 14.95;
+
+    cout << "The company's profit would be " << cost * PERCENT_PROFIT << endl;
+
+    return 0;
+}
---
+++ startingOut/ch2/14_personalInformation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Name: Timothy Diliberto\n"
+         << "Address: 1662 Dwight Way Unit A, Berkeley, CA 94703\n"
+         << "Phone #: +1 (559) 213-4959\n"
+         << "Major: Data Science\n";
+
+    return 0;
+}
