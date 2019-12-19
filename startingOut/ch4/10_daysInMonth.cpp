cfbd2a12157cf1c5d15aaf723f8b76d797311aac
Timothy <tdiliberto19@berkeley.edu>
2019-12-19 14:55:27 -0800

    started 10

---
+++ startingOut/ch4/10_daysInMonth.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int year;
+    string month;
+
+    cout << "Please enter a numeric month (1-12), and the year: ";
+    cin >> month >> year;
