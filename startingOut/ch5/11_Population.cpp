792e460cece8389ee0377f7fda4f7998defbcec6
Timothy <tdiliberto19@berkeley.edu>
2020-03-13 03:59:12 -0700

    finished 11

--- startingOut/ch5/11_Population.cpp
+++ startingOut/ch5/11_Population.cpp
@@ -1,1 +1,33 @@
+/**
+    Obtains information about the number of base organisms as well as their growth rate and the
+    number of days observed. The program then outputs the total population for each day of the
+    study.
+    
+    @author Timothy Diliberto
+    @version 1.0 3/12/2020
+*/
+
 #include <iostream>
+
+using namespace std;
+
+int main() {
+    int organisms,
+        days;
+    double percent_increase;
+
+    cout << "Enter the starting number of organisms: ";
+    cin >> organisms;
+    cout << "Enter their daily population increase (as a percentage between 0 and 100): ";
+    cin >> percent_increase;
+    cout << "Enter the number of days the organisms' growth was observed: ";
+    cin >> days;
+    
+    cout << "Day 0: " << organisms << " organisms\n";
+    for (int day = 1; day <= days; day++) {
+        organisms *= 1.0 + percent_increase / 100.0;
+        cout << "Day " << day << ": " << organisms << " organisms\n";
+    }
+
+    return 0;
+}
